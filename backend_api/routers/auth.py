ee86c93c98331018b39ab6e261ba37e80a600e08
Tenzin <tenzinwang@itechnologies.bt>
2025-12-24 12:57:33 +0600

    password length manupulation

--- backend_api/routers/auth.py
+++ backend_api/routers/auth.py
@@ -26,12 +26,11 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 # Password helpers
 # -------------------------------------------------------
 def get_password_hash(password: str):
-    return pwd_context.hash(password)
+    return pwd_context.hash(password[:72])
 
 
 def verify_password(plain_password: str, hashed_password: str):
-    return pwd_context.verify(plain_password, hashed_password)
-
+    return pwd_context.verify(plain_password[:72], hashed_password)
 
 # -------------------------------------------------------
 # JWT helpers
